dcd76fb86a307acdc388893937426f20efb2219d
Leandro Lucarella <luca-frequenz@llucax.com>
2024-12-10 14:07:47 +0100

    Revert "Add a global instance for the config manager"
    It seems to be more clear and easier to test to always pass a config
    manager explicitly, so we just remove the global instance, which was
    also a bit confusing when presented.
    
    This reverts commit 77295dcf6ea3f5cd79472fc3fa4e94cc860a21db.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/sdk/config/__init__.py
+++ src/frequenz/sdk/config/__init__.py
@@ -3,11 +3,6 @@
 
 """Configuration management."""
 
-from ._global import (
-    get_config_manager,
-    initialize_config_manager,
-    shutdown_config_manager,
-)
 from ._logging_actor import LoggerConfig, LoggingConfig, LoggingConfigUpdatingActor
 from ._manager import ConfigManager
 from ._managing_actor import ConfigManagingActor
@@ -19,8 +14,5 @@ __all__ = [
     "LoggerConfig",
     "LoggingConfig",
     "LoggingConfigUpdatingActor",
-    "get_config_manager",
-    "initialize_config_manager",
     "load_config",
-    "shutdown_config_manager",
 ]
